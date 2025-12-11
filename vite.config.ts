76e5977713c18879007da67df556220f3a2ccc16
Omid <omidajorlou@yahoo.com>
2025-12-11 08:50:06 +0330

    Add base URI for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  
   const env = loadEnv(mode, (process as any).cwd(), '');
   
   return {
+    base: '/Hikaru-Studio/',
     plugins: [react()],
     define: {
       // Safely expose only the API_KEY from the loaded environment
